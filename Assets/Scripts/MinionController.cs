3a6bfe2ffb95e9df9d15d870babda37b5b92dbd9
Ahmed-ShawkyEgy <ahmed.shawkyegy@gmail.com>
2019-07-02 20:03:28 +0200

    add minionController and moveTowards function

---
+++ Assets/Scripts/MinionController.cs
@@ -0,0 +1,36 @@
+﻿
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Minion : MonoBehaviour
+{
+    public int health;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    void receiveDamage(int damageAmount)
+    {
+        health -= damageAmount;
+        if (health <= 0)
+            Destroy(this);
+    }
+
+    void OnCollisionEnter(Collision collision)
+    {
+        if(collision.gameObject.tag == "MasterMind")
+        {
+            // TODO increase the score of the masterMind
+        }
+    }
+}
