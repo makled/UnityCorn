e5375d68faee5a888af539ab7ddbd635b1c8f167
EkinTiras <tiras@conenergy.com>
2019-07-03 13:58:44 +0200

    included spawner

--- Assets/Scripts/SpawnManager.cs
+++ Assets/Scripts/SpawnManager.cs
@@ -5,4 +5,13 @@ using UnityEngine;
 public class SpawnManager : MonoBehaviour
 {
     
+    void SpawnInitialization(System.Tuple<GameObject, GameObject>[] selectedEnemiesAndSpawns)
+    {
+        foreach(System.Tuple<GameObject, GameObject> enemyAndSpawn in selectedEnemiesAndSpawns)
+        {
+            Spawner spawnerScript = enemyAndSpawn.Item2.GetComponent<Spawner>();
+            GameObject enemy = enemyAndSpawn.Item1;
+            spawnerScript.Spawn(enemy);
+        }
+    }
 }
