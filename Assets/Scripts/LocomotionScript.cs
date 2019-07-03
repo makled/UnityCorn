c4989bb4a0e430cde72b02fea21b08fb825d3dcf
Tristan Kley <tristan.kley@paluno.uni-due.de>
2019-07-03 10:42:33 +0200

    dash should work now and we have an arena prefab

--- Assets/Scripts/LocomotionScript.cs
+++ Assets/Scripts/LocomotionScript.cs
@@ -7,8 +7,13 @@ namespace Unitycorn
     public class LocomotionScript : MonoBehaviour
     {
 
-        private static float closeToMaxStickMagnitude = 0.8f;
-        private static float closeToMinStickMagnitude = 0.2f;
+        private static float CLOSE_TO_MAX_STICK_MAGNITUDE = 0.8f;
+        private static float CLOSE_TO_MIN_STICK_MAGNITUDE = 0.5f;
+        private static float LOCOMOTION_FORCE = 0.03f;
+        private static float MOMENTUM_PART = 0.66f;
+        private static int DASH_TIME_MILLIS = 400;
+        private static int FLICK_TOLERANCE_MILLIS = 200;
+
 
         [SerializeField]
         private GameObject player;
@@ -17,7 +22,8 @@ namespace Unitycorn
         private System.DateTime lastCheck;
         private System.DateTime lastDash;
         private System.DateTime lastRegularLocomotion;
-        private Vector2 lastVector;
+        private Vector2 lastCheckVector;
+        private Vector2 lastLocomotionVector;
 
 
 
@@ -29,6 +35,7 @@ namespace Unitycorn
             lastDash = System.DateTime.MinValue;
 
             lastRegularLocomotion = System.DateTime.Now;
+            lastLocomotionVector = new Vector2();
         }
 
         // Update is called once per frame
@@ -39,11 +46,11 @@ namespace Unitycorn
 
         public void supplyLocomotionVector(Vector2 vector)
         {
-            if (vector.magnitude > closeToMaxStickMagnitude)
+            if (vector.magnitude > CLOSE_TO_MAX_STICK_MAGNITUDE)
             {
-                if(System.DateTime.Now.Subtract(lastCheck) < new System.TimeSpan(0, 0, 0, 0, 100) && lastVector.magnitude < closeToMinStickMagnitude)
+                if(System.DateTime.Now.Subtract(lastCheck) < new System.TimeSpan(0, 0, 0, 0, FLICK_TOLERANCE_MILLIS) && lastCheckVector.magnitude < CLOSE_TO_MIN_STICK_MAGNITUDE)
                 {
-                    if(System.DateTime.Now.Subtract(lastFlick) < new System.TimeSpan(0, 0, 0, 0, 200))
+                    if(System.DateTime.Now.Subtract(lastFlick) < new System.TimeSpan(0, 0, 0, 0, FLICK_TOLERANCE_MILLIS * 2))
                     {
                         applyDash(vector);
                         lastDash = System.DateTime.Now; 
@@ -51,12 +58,12 @@ namespace Unitycorn
                     lastFlick = System.DateTime.Now;
                 }
                 lastCheck = System.DateTime.Now;
-                lastVector = vector;
+                lastCheckVector = vector;
             }
-            if(vector.magnitude < closeToMinStickMagnitude)
+            if(vector.magnitude < CLOSE_TO_MIN_STICK_MAGNITUDE)
             {
                 lastCheck = System.DateTime.Now;
-                lastVector = vector;
+                lastCheckVector = vector;
             }
             
             applyRegularLocomotion(vector);
@@ -66,15 +73,23 @@ namespace Unitycorn
 
         private void applyDash(Vector2 vector)
         {
+            lastLocomotionVector = vector;
             Debug.Log("Dash!");
         }
 
         private void applyRegularLocomotion(Vector2 vector)
         {
+            Vector2 locomotion = lastLocomotionVector * MOMENTUM_PART + vector * (1f-MOMENTUM_PART);
+            if(System.DateTime.Now.Subtract(lastDash) < new System.TimeSpan(0, 0, 0, 0, DASH_TIME_MILLIS))
+            {
+                locomotion = locomotion * 10f;
+            }
+
+            lastLocomotionVector = locomotion;
             while(lastRegularLocomotion<System.DateTime.Now)
             {
                 lastRegularLocomotion = lastRegularLocomotion + new System.TimeSpan(0, 0, 0, 0, 8);
-                player.transform.position = player.transform.position - new Vector3(vector.x, 0, vector.y) * 0.03f;
+                player.transform.position = player.transform.position - new Vector3(locomotion.x, 0, locomotion.y) * LOCOMOTION_FORCE;
             }
         }
     }
