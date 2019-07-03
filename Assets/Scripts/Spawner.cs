dee7d50c58ec608d2cfa8f487ff66bdc5215af0f
EkinTiras <tiras@conenergy.com>
2019-07-03 10:32:48 +0200

    Spawner Script and SpawnManager

---
+++ Assets/Scripts/Spawner.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawner : MonoBehaviour
+{
+
+    public void Spawn(GameObject enemy)
+    {
+        Instantiate(enemy, transform.position, Quaternion.identity);
+    }
+}
